1f6bbc6334d5b18cb5c023ffd7286448c565cf8e
Renzo Telenta <renzotelenta@gmail.com>
2021-03-07 17:02:30 -0500

    fix searcher

--- features/Dashboard/components/Finder.tsx
+++ features/Dashboard/components/Finder.tsx
@@ -1,40 +1,45 @@
 import { Box } from "@chakra-ui/layout";
-import { Heading, Text } from "@chakra-ui/react";
-import AsyncSelect from "react-select/async";
+import { Heading, Skeleton, Text } from "@chakra-ui/react";
+import Select from "react-select";
 import React from "react";
 import getPlaces from "../services/getPlaces";
+import { useQuery } from "react-query";
 
 const Finder: React.FC = () => {
+  const { data: places, isLoading } = useQuery("places", getPlaces);
+
   return (
     <Box mt="20">
       <Heading as="h2" color="gray.600" mb="8" fontWeight="bold" fontSize="xl">
         Ya empezamos la carrera por la vacunación
       </Heading>
 
-      <AsyncSelect
-        styles={{
-          control: (base) => ({
-            ...base,
-            border: 0,
-            borderRadius: 12,
-            fontSize: 14,
-            padding: "6px 8px",
-          }),
-        }}
-        theme={(theme) => ({
-          ...theme,
-          borderRadius: 0,
-          colors: {
-            ...theme.colors,
-            primary: "#0BC5EA",
-          },
-        })}
-        cacheOptions
-        loadOptions={getPlaces}
-        defaultOptions
-        onInputChange={(val) => console.log(val)}
-        placeholder="Todo el Perú"
-      />
+      {isLoading ? (
+        <Skeleton w="full" height="50px" />
+      ) : (
+        <Select
+          styles={{
+            control: (base) => ({
+              ...base,
+              border: 0,
+              borderRadius: 12,
+              fontSize: 14,
+              padding: "6px 8px",
+            }),
+          }}
+          theme={(theme) => ({
+            ...theme,
+            borderRadius: 0,
+            colors: {
+              ...theme.colors,
+              primary: "#0BC5EA",
+            },
+          })}
+          options={places}
+          onInputChange={(val) => console.log(val)}
+          placeholder="Todo el Perú"
+        />
+      )}
 
       <Text fontSize="xs" mt="2" ml="4">
         Buscar por departamento o distrito. Ejemplo: Santa Anita
